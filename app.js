449a02a7826f582be57c152afbc49b014c60da79
YouDevIt <32442080+YouDevIt@users.noreply.github.com>
2021-07-19 00:17:16 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,55 @@
 
-text_en={
-  nospecial: "You see nothing special."
+system_enUS={
+  Nospecial:"You see nothing special.",
+  Inventory:"Inventory",
+  Carrynothing:"You carry nothing."
 }
-text_it={
-  nospecial: "Non noti nulla di speciale."
+system_it={
+  Nospecial:"Non noti nulla di speciale",
+  Inventory:"Inventario",
+  Carrynothing:"Non porti nulla."
 }
-var text=text_en
+var system=system_enUS
+
+story_enUS={
+  kitchen:"kitchen",
+  kitchendesc:"A large kitchen in which you see a {table}.",
+  pen:'pen',
+  table:'table',
+  box:'box',
+}
+story_it={
+  kitchen:'cucina',
+  kitchendesc:"Un'ampia cucina in cui vedi un {table}.",
+  pen:'penna',
+  table:'tavolo',
+  box:'scatola',
+}
+var story=story_enUS
+
+function changelang(code){
+  switch(String(code)){
+    default:
+    case 'us':
+      choutline('us',"solid 2px red;")
+      choutline('it',"none;")
+      system=system_enUS
+      story=story_enUS
+      break
+    case 'it':
+      choutline('it',"solid 2px red;")
+      choutline('us',"none;")
+      system=system_it
+      story=story_it
+      break
+  }
+  refresh()
+}
+function choutline(el,newstr){
+  var str=getel(el).style.cssText
+  var idx= str.indexOf("outline:")
+  getel(el).style.cssText=str.substring(0,idx+9)+newstr
+}
 
 function world(start){
   this.objects={}
@@ -18,7 +62,7 @@ function object(id,type){
   this.id=id
   this.type=type?type:'object'
   this.name=id
-  this.desc=text.nospecial
+  this.desc=system.Nospecial
   this.loc=''
 }
 function group(id,type){
@@ -47,13 +91,12 @@ function supporter(id){
   this.enterable=true
 }
 
-var $=new world('cucina')
-var cucina=new room('cucina')
-cucina.desc="Un'ampia cucina."
-$.add(cucina)
-$.add(new thing('penna'))
-$.add(new supporter('tavolo'))
-$.add(new container('scatola'))
+function getel(elname){
+  return document.getElementById(elname);
+}
+function inner(elname,text){
+  getel(elname).innerHTML=text
+}
 
 function allowDrop(ev) {
   ev.preventDefault();
@@ -67,7 +110,7 @@ function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   ev.target.innerHTML += data;
-  document.getElementById('message').innerHTML=data+' '+text.nospecial
+  document.getElementById('message').innerHTML=data+' '+system.Nospecial
 }
 
 function cap(txt){
@@ -78,20 +121,26 @@ function print(txt){
 }
 
 function exp(obj) {
-  print(inner(obj.type,obj.name))
+  print(innerobject(obj.type,obj.name))
 }
 
-function inner(type,name) {
+function innerobject(type,name) {
   return '<span class="'+type+'" id="div1" draggable="true" ondragstart="drag(event)" ondrop="drop(event)" ondragover="allowDrop(event)">'+name+'</span>';
 }
 
+function markText(text){
+  text=text.replaceAll(/{(\w+)}/g,function(match,p1){
+    return innerobject($.objects[p1].type,story[$.objects[p1].name]);
+  })
+  return text
+}
 function changeEditable() {
   document.body.contentEditable = document.body.contentEditable=='true'? 'false': 'true';
   if(document.body.contentEditable == 'false') {
     var markup = document.documentElement.innerHTML;
-    markup = markup.replaceAll(/{t (\w+)}/g,function(match,p1){return inner("thing",p1);});
-    markup = markup.replaceAll(/{s (\w+)}/g,function(match,p1){return inner("supporter",p1);});
-    markup = markup.replaceAll(/{c (\w+)}/g,function(match,p1){return inner("container",p1);});
+    markup = markup.replaceAll(/{t (\w+)}/g,function(match,p1){return innerobject("thing",p1);});
+    markup = markup.replaceAll(/{s (\w+)}/g,function(match,p1){return innerobject("supporter",p1);});
+    markup = markup.replaceAll(/{c (\w+)}/g,function(match,p1){return innerobject("container",p1);});
     document.documentElement.innerHTML = markup;
   }
 }
@@ -105,3 +154,26 @@ function download(exportText){
   downloadAnchorNode.click();
   downloadAnchorNode.remove();
 }
+
+window.onload=function(){
+ refresh()
+}
+
+function refresh(){
+ inner('room',
+    "<h2>"+cap(story[$.objects[$.start].name])+"</h2>"
+    +markText(story[$.objects[$.start].desc])
+  )
+  inner('inventory',
+    "<h2>"+system.Inventory+"</h2>"
+    +system.Carrynothing
+  )
+}
+
+var $=new world('kitchen')
+var r=new room('kitchen')
+r.desc='kitchendesc'
+$.add(r)
+$.add(new thing('pen'))
+$.add(new supporter('table'))
+$.add(new container('box'))
